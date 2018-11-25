11c94c785059c54f79ba88f5ed909ccf86fea89a
wjunjie83 <wjunjie@126.com>
2018-11-25 11:58:18 +0800

    Site updated: 2018-11-25 11:58:12

--- 2018/11/25/瓦力工厂/DPJ-047-鳄鱼机器人/resize.sh
+++ 2018/11/25/瓦力工厂/DPJ-047-鳄鱼机器人/resize.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo "resize image who is bigger than 200k";
-for i in `find . -size +200k`;
+echo "resize image who is bigger than 500k";
+for i in `find . -size +500k`;
 do
 convert $i -resize 35% $i;
 echo "resize image $i to 35%";
