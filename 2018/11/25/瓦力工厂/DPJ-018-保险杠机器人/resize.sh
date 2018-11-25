afc4901b19a3546d083addcb578985e3f2d8e711
wjunjie83 <wjunjie@126.com>
2018-11-25 11:27:28 +0800

    Site updated: 2018-11-25 11:27:20

--- 2018/11/25/瓦力工厂/DPJ-018-保险杠机器人/resize.sh
+++ 2018/11/25/瓦力工厂/DPJ-018-保险杠机器人/resize.sh
@@ -2,6 +2,6 @@
 echo "resize image who is bigger than 200k";
 for i in `find . -size +200k`;
 do
-convert $i -resize 15% $i;
-echo "resize image $i to 25%";
+convert $i -resize 35% $i;
+echo "resize image $i to 35%";
 done
